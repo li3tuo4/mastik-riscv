fdbd312938100343ba2593702e0d8b6810a26dfd
Clifford Sesel <clifford@unsw.edu.au>
2021-01-25 09:29:15 +1100

    Paramaterized L1-capture and L1-rattle
    L1 capture now takes an optional additional argument that changes the delay
    loop.
    L1-Rattle takes 3 arguments. Two values for the locations to modify and 1
    for the number of times to modify them

--- demo/L1-rattle.c
+++ demo/L1-rattle.c
@@ -5,10 +5,38 @@
 volatile char buffer[4096];
 
 int main(int ac, char **av) {
+  //default values from original mastic repo
+  int rattle_1 = 800;
+  int rattle_2 = 1800;
+  int rattle_times = 64000;
+
+  //if 2 arguments are given that are valid integers between 0 and 4096 use them instead of default values
+  if (ac >= 3){
+    int r_1 = atoi(av[1]);
+    int r_2 = atoi(av[2]);
+
+
+    if (r_1 >= 0 && r_1 <4096){
+      rattle_1 = r_1;
+    }
+    if (r_2 >= 0 && r_2 <4096){
+      rattle_2 = r_2;
+    }
+
+    if (ac == 4){
+      int r_t = atoi(av[3]);
+      if (r_t > 0){
+        rattle_times = r_t;
+      }
+    }
+
+  }
+  fprintf(stderr, "using rattle values: %d and %d, %d times\n", rattle_1, rattle_2, rattle_times);
+
   for (;;) {
-    for (int i = 0; i < 64000; i++)
-      buffer[800] += i;
-    for (int i = 0; i < 64000; i++)
-      buffer[1800] += i;
+    for (int i = 0; i < rattle_times; i++)
+      buffer[rattle_1] += i;
+    for (int i = 0; i < rattle_times; i++)
+      buffer[rattle_2] += i;
   }
 }
